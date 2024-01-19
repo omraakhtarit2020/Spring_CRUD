7827a8e47253edbd645681644fb95efa1a5a28a4
omra akhtar <umraakhtar786@gmail.com>
2024-01-19 20:12:49 +0530

    Error solved

--- src/main/java/com/myfirstspringapp/myFirstApp/payload/ApiResponse.java
+++ src/main/java/com/myfirstspringapp/myFirstApp/payload/ApiResponse.java
@@ -3,6 +3,23 @@ package com.myfirstspringapp.myFirstApp.payload;
 public class ApiResponse {
 
 	private String message;
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public boolean isSuccess() {
+		return success;
+	}
+
+	public void setSuccess(boolean success) {
+		this.success = success;
+	}
+
 	private boolean success;
 
 	public ApiResponse(String message, boolean success) {
